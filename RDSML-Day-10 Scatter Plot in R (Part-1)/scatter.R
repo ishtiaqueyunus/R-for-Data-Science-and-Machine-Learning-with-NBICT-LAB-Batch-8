ee2b313e180896975d0e3536fe19f575c61677ba
ishtiaqueyunus <ishtiaque.yunus@gmail.com>
2025-05-28 22:39:40 +0600

    scatter_part1

---
+++ RDSML-Day-10 Scatter Plot in R (Part-1)/scatter.R
@@ -0,0 +1,20 @@
+# install.packages("ggplot2")
+library(ggplot2)
+
+dataset = mtcars
+
+# Generating a basic scatter plot
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point()
+
+# Changing the point size and shape
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point(size = 1, shape = 18)
+
+# Adding the regression line
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point(size = 1, shape = 18) +
+  geom_smooth(method ="lm" ) +
+  labs(title = "Scatterplot of a car wt vs car mpg", 
+       x = "Wt of Cars (pound)",
+       y = "Miles per Gallon (Gallon)")
