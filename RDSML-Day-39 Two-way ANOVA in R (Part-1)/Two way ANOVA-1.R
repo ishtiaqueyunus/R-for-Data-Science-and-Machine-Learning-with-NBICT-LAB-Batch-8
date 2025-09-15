b11d1637566b5ff2a1b207ed3d6b36d668862a0f
ishtiaqueyunus <ishtiaque.yunus@gmail.com>
2025-09-15 22:29:52 +0600

    Day 39

---
+++ RDSML-Day-39 Two-way ANOVA in R (Part-1)/Two way ANOVA-1.R
@@ -0,0 +1,6 @@
+# installing packages
+install.packages(c("tidyverse", "car", "emmeans", "effectsize", "performance", "broom", "kableExtra"))
+library(tidyverse)
+
+# Importing the dataset
+d <- read.csv("cropdata.csv")
